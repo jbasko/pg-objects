5fa037189c7498d62740a91bec939f5f9656dff9
Jazeps Basko <jazeps.basko@gmail.com>
2019-05-14 11:08:04 +0100

    Add a test example for DefaultPrivilege

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,6 +1,8 @@
 from unittest import mock
 
 from pg_objects.objects.database import DatabasePrivilege
+from pg_objects.objects.default_privilege import DefaultPrivilege, DefaultPrivilegeReady
+from pg_objects.objects.schema import SchemaTablesPrivilege
 from pg_objects.setup import Setup
 
 
@@ -29,3 +31,19 @@ def test_database_privilege():
 
     dp = DatabasePrivilege("db", "rol", privileges=["CONNECT", "TEMP"])
     assert dp.privileges == {"CONNECT", "TEMPORARY"}
+
+
+def test_default_privilege_example():
+    dp = DefaultPrivilege(
+        grantor="postgres",
+        privilege=SchemaTablesPrivilege(
+            database="db",
+            schema="sch",
+            grantee="datascience",
+            privileges="ALL",
+        ),
+    )
+
+    assert dp.privilege.database == "db"
+    assert dp.privilege.schema == "sch"
+    assert dp.privilege.grantee == "datascience"
